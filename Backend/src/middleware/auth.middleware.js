06c945dbf3a7fbd1ca379a69dfd951c6655b58f3
vishalnaidu77 <vminorr@gmail.com>
2026-06-22 12:56:04 +0530

    feat: implement user authentication, password management, and authorization middleware

---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+export async function veriifyUser(req, res, next) {
+    const { token } = req.cookies
+
+    if(!token){
+        return res.status(401).json({
+            message: "Token missing, user not logged in",
+            success: false,
+            err: "Token missing"
+        })
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(!decoded.id){
+            return res.status(401).json({
+                message: "Unauthorized token",
+                success: false,
+                err: "Unauthorized"
+            })
+        }
+
+        req.userId = decoded.id
+        next()
+    } catch (err) {
+        return res.status(400).json({
+            message: "Unexpected error",
+            success: false,
+            err: err.message
+        })
+    }
+}
