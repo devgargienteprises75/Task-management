566c15c86a69331920aa030b68680a319713ce60
vishalnaidu77 <vminorr@gmail.com>
2026-07-03 14:02:44 +0530

    feat: implement task detail endpoints for workspace management

--- Backend/src/routes/task.routes.js
+++ Backend/src/routes/task.routes.js
@@ -1,18 +1,18 @@
 import { Router } from "express";
 import { requireAdminOrHead, verifyWorkspaceOwnership, verifyWorkspaceUser } from "../middleware/workspace.middleware.js";
-import { createTaskController, getTasksController } from "../controller/task.controller.js";
+import { createTaskController, getTasksController, getTaskDetailController } from "../controller/task.controller.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
 
 const taskRouter = Router()
 
 // Manage Tasks
-taskRouter.post(
-    "/:workspaceid/create-task", 
-    requireAdminOrHead, 
-    verifyWorkspaceOwnership, 
-    createTaskController
-)
+
+// Assign new Task
+taskRouter.post("/:workspaceid/create-task", requireAdminOrHead, verifyWorkspaceOwnership, createTaskController)
+
+// Fetch all task of workspace
 taskRouter.get("/:workspaceid/tasks", verifyUser, verifyWorkspaceUser, getTasksController)
 
-
+// Fetch single task detail
+taskRouter.get("/:workspaceid/task-details/:taskid", verifyUser, verifyWorkspaceUser, getTaskDetailController)
 export default taskRouter
