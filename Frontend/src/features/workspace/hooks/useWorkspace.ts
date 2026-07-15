56913222edbf1c7092273d6fa32905806413e8af
Vishal <vminorr@gmail.com>
2026-07-15 17:09:06 +0530

    fix: type error in workspace

--- Frontend/src/features/workspace/hooks/useWorkspace.ts
+++ Frontend/src/features/workspace/hooks/useWorkspace.ts
@@ -4,57 +4,56 @@ import { useDispatch } from "react-redux";
 import { setAllWorkspaces, setLoading, setWorkspace } from "../workspace.slice";
 
 const useWorkspace = () => {
+  const { createWorkspace, getWorkspaces } = workspaceApi;
+  const dispatch = useDispatch();
 
-    const { createWorkspace, getWorkspaces } = workspaceApi
-    const dispatch = useDispatch()
+  const handleCreateWorkspace = async (workspaceDetail: workspace) => {
+    dispatch(setLoading(true));
 
-    const handleCreateWorkspace = async (workspaceDetail: workspace) => {
-        dispatch(setLoading(true))
-
-        try {
-            const res = await createWorkspace(workspaceDetail)
-            dispatch(setWorkspace(res.workspace))
-            dispatch(setAllWorkspaces(res.workspace))
-            return {
-                success: true,
-                message: res.message
-            }
-        } catch (err: any) {
-            const message = err?.response?.data?.message || err.message
-            return {
-                success: false,
-                message
-            }
-        } finally {
-            dispatch(setLoading(false))
-        }
+    try {
+      const res = await createWorkspace(workspaceDetail);
+      dispatch(setWorkspace(res.workspace));
+      dispatch(setAllWorkspaces([res.workspace]));
+      return {
+        success: true,
+        message: res.message,
+      };
+    } catch (err: any) {
+      const message = err?.response?.data?.message || err.message;
+      return {
+        success: false,
+        message,
+      };
+    } finally {
+      dispatch(setLoading(false));
     }
+  };
 
-    const handleGetWorkspaces = async () => {
-        dispatch(setLoading(true))
+  const handleGetWorkspaces = async () => {
+    dispatch(setLoading(true));
 
-        try {
-            const res = await getWorkspaces()
-            dispatch(setAllWorkspaces(res.workspaces))
-            return {
-                success: true,
-                message: res.message
-            }
-        } catch (err: any) {
-            const message = err?.response?.data?.message || err.message
-            return {
-                success: false,
-                message
-            }
-        } finally {
-            dispatch(setLoading(false))
-        }
+    try {
+      const res = await getWorkspaces();
+      dispatch(setAllWorkspaces(res.workspaces));
+      return {
+        success: true,
+        message: res.message,
+      };
+    } catch (err: any) {
+      const message = err?.response?.data?.message || err.message;
+      return {
+        success: false,
+        message,
+      };
+    } finally {
+      dispatch(setLoading(false));
     }
+  };
 
-    return {
-        handleCreateWorkspace,
-        handleGetWorkspaces
-    }
-}
+  return {
+    handleCreateWorkspace,
+    handleGetWorkspaces,
+  };
+};
 
 export default useWorkspace;
